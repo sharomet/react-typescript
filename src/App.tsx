5383e6c57e56b48a7507f05b6f711d1309f56dd6
asharomet <sharometal@gmail.com>
2020-08-19 16:00:17 +0300

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header'
 
-//import { RouteComponentProps } from 'react-router'
-
-/*type RouteParams = {
-  id: string
-}
-*/
-//RouteComponentProps<RouteParams>
-//const id = this.props.match.param.id || '';
-
 const App: React.FC = ({ children }) => (
   <div className="wrapper">
     <Header/>
